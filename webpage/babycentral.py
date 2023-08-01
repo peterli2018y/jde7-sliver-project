27a63acb469701dec23d86d4263a93b696980e4d
peterli2018y <peterli2018y@gmail.com>
2023-08-01 15:25:57 +0800

    import venv

--- webpage/babycentral.py
+++ webpage/babycentral.py
@@ -1,1 +1,12 @@
-url=f"https://babycentral.com.hk/zh/search?q={searchInfo}"
+from my_selenium import openWedsite
+from selenium.webdriver.common.by import By
+
+def search(search_Info):
+    url=f"https://babycentral.com.hk/zh/search?q={search_Info}&bbchk_products%5BsortBy%5D=bbchk_products_price_high_to_low"
+    driver=openWedsite(url)
+    return driver
+
+def run():
+    driver = search("奶粉")
+
+    result04=d.find_elements(By.XPATH, "//li")
--- webpage/ddd.py
+++
@@ -1,5 +0,0 @@
-from my_selenium import openWedsite
-def run():
-    search_Info ="奶粉"
-    url=f"https://babycentral.com.hk/zh/search?q={search_Info}" 
-    openWedsite(url)
